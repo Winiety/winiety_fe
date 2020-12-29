00aecf92966bc6326ffa3712e42c67b844a997ce
Rafał Plinzner <rplinzner@gmail.com>
2020-12-29 20:14:39 +0100

    [Add] easy-peasy logic

--- winiety_web_app/src/layout/index.tsx
+++ winiety_web_app/src/layout/index.tsx
@@ -1,5 +1,7 @@
-import { makeStyles, Theme } from '@material-ui/core';
+import { makeStyles, MuiThemeProvider, Theme } from '@material-ui/core';
 import React, { ReactElement, useRef } from 'react';
+import { dark, light } from 'themes';
+import { useStoreState } from 'store';
 import AppBar from './AppBar';
 import BottomNavigationComp from './BottomNavigation';
 
@@ -25,23 +27,28 @@ interface Props {
 export default (props: Props): ReactElement => {
   const { children } = props;
   const classes = useStyles();
+
+  const isDarkTheme = useStoreState((state) => state.userSettings.isDarkTheme);
+
   const bottomNavigationElement = useRef(
     document.getElementById('bottom__nav')
   );
   return (
-    <div className={classes.root}>
-      <AppBar />
-      <div className={classes.offset} />
-      <div
-        style={{
-          marginBottom: bottomNavigationElement.current?.clientHeight || 56,
-        }}
-        id="app__content"
-        className="h-100 overflow-auto"
-      >
-        {children}
+    <MuiThemeProvider theme={isDarkTheme ? dark : light}>
+      <div className={classes.root}>
+        <AppBar />
+        <div className={classes.offset} />
+        <div
+          style={{
+            marginBottom: bottomNavigationElement.current?.clientHeight || 56,
+          }}
+          id="app__content"
+          className="h-100 overflow-auto"
+        >
+          {children}
+        </div>
+        <BottomNavigationComp className={classes.stickToBottom} />
       </div>
-      <BottomNavigationComp className={classes.stickToBottom} />
-    </div>
+    </MuiThemeProvider>
   );
 };
