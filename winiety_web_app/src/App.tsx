69e595013135f21449a3e5259bf5c65ceb269a0e
Rafał Plinzner <rplinzner@gmail.com>
2021-02-08 21:24:52 +0100

    add history listen

--- winiety_web_app/src/App.tsx
+++ winiety_web_app/src/App.tsx
@@ -2,7 +2,7 @@ import React, { ReactElement, useEffect } from 'react';
 import { RoutedContent } from 'routes';
 import { CssBaseline } from '@material-ui/core';
 import { useStoreRehydrated } from 'easy-peasy';
-import { BrowserRouter, useHistory } from 'react-router-dom';
+import { BrowserRouter } from 'react-router-dom';
 import Layout from 'layout';
 import { checkSupport, requestNotificationPermission } from 'utils';
 import ReactGA from 'react-ga';
@@ -15,17 +15,11 @@ ReactGA.set({ cd1: 'online' });
 
 const App = (): ReactElement => {
   const isRehydrated = useStoreRehydrated();
-  const history = useHistory();
 
   useEffect(() => {
     if (!checkSupport()) return;
     requestNotificationPermission();
   }, []);
-
-  history.listen(({ location }) => {
-    ReactGA.set({ page: location.pathname });
-    ReactGA.pageview(location.pathname);
-  });
 
   return (
     <>
